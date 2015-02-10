df3af24f73943471ca84c5827d60dc1ee8c4381d
Dotun Olusoga <adedotun.olusoga@gmail.com>
2015-02-10 06:58:54 -0600

    html boilerplate

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,43 @@
-
+'use strict';
 
 function hello() {
   return 'world';
+}
+
+var url = 'https://friends-list.firebaseio.com/.json';
+
+$(document).ready(init);
+
+function init(){
+  hideForm();
+
+$('#contactForm').click(revealForm);
+
+$('#submitButton').click(submitForm);
+}
+
+function submitForm(event){
+  event.preventDefault();
+
+  var $name      = $('#contactName').val();
+  var $phone     = $('#contactPhone').val();
+  var $twitter   = $('#twitterHandle').val();
+  var $instagram = $('#instagram').val();
+  var $photo     = $('#photoUrl').val();
+
+  var contacts = { name: $name, phone: $phone, twitter: $twitter, instagram: $instagram, photo: $photo };
+  var contactList = JSON.stringify(contacts);
+  $.post(url, contactList, function(res){
+  });
+
+};
+
+function hideForm() {
+  var $friendsForm = $('#friendsForm').hide();
+  return $friendsForm;
+};
+
+function revealForm(){
+  var $friendsForm = $('#friendsForm').show();
+  return $friendsForm;
 }
