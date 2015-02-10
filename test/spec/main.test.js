3141f7714a2fdc2acea31e3f7b22d10c0ff66ac9
Dotun Olusoga <adedotun.olusoga@gmail.com>
2015-02-09 18:25:02 -0600

    test for hello world function

--- test/spec/main.test.js
+++ test/spec/main.test.js
@@ -1,7 +1,14 @@
+'use strict';
 
 describe('test suite', function () {
   it('should assert true', function () {
     true.should.be.true;
     false.should.be.false;
+  });
+});
+
+describe('hello', function () {
+  it('should return world', function () {
+    hello().should.equal('world');
   });
 });
